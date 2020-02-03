cff81c401471989d76ef2055954b3570826b6855
Will Corcoran <corcoranwill@gmail.com>
2020-02-03 21:47:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,10 @@
 def fizzbuzz(int)
   count == 0
   if int % 3 == 0
-    count +=1
     "Fizz"
   elsif int % 5 == 0
-    count += 1
     "Buzz"
-  elsif count == 2 #int % 3 == 0 && int % 5 == 0
+  elsif int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   else
     nil
