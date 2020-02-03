75643394f8c7942ec9b1816c9add1c5eee9810d9
Will Corcoran <corcoranwill@gmail.com>
2020-02-03 21:51:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,9 @@
 def fizzbuzz(int)
   if int % 15 == 0
     "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
   end
-
-  # if int % 3 == 0
-  #   "Fizz"
-  # elsif int % 5 == 0
-  #   "Buzz"
-  # end
 end
