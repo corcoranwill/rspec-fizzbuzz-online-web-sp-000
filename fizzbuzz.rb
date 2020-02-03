bdb1a5493f722ff938e76167e79dc34460247d8a
Will Corcoran <corcoranwill@gmail.com>
2020-02-03 21:46:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
+  count == 0
   if int % 3 == 0
     "Fizz"
+    count +=1
   elsif int % 5 == 0
       "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0
+      count += 1
+  elsif count == 2 #int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   else
     nil
